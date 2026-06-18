2923170319eda72438aabe46f2f7b5f74459591b
Neha Sapra <nehasapra1995@gmail.com>
2026-06-18 16:14:56 +0530

    Bank account.py

--- Calculator.py
+++ Calculator.py
@@ -1,20 +1,14 @@
 class Calculator:
-    
-   def add(self,a,b):
-     return a + b
-   
-   def subtract(self,a,b):
-     return a - b
-   
-   def multiply(self,a,b):
-     return a * b
-   
-   def divide(self,a,b):
-    return a / b
-   
-   cal = calculator()
+    def add(self, a, b): return a + b
+    def subtract(self, a, b): return a - b
+    def multiply(self, a, b): return a * b
+    def divide(self, a, b):
+        if b == 0:
+            return "Error: Division by zero"
+        return a / b
 
-   print(cal.add(10,5))
-   print(cal.subtract(10,5))                            
-   print(cal.multiply(10,5))
-   print(cal.divide(10,5))
+calc = Calculator()
+print(calc.add(10, 5))
+print(calc.subtract(10, 5))
+print(calc.multiply(10, 5))
+print(calc.divide(10, 5))
